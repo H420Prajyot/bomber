d59b59286902fa6825c45cf211dffe57d27797a3
H420 <60284945+H420Prajyot@users.noreply.github.com>
2020-07-22 23:22:55 -0700

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 clear
 echo -e "\e[4;31m SpeedX Productions !!! \e[0m"
+echo -e "\e[4;30m MODIFIED BY H420 \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
 echo -e "\e[1;32m TBomb \e[0m"
 echo "Press Enter To Continue"
@@ -14,7 +15,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By SpeedX >update.speedx
+echo This modified Script Was Made By H420 >update.speedx
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -27,7 +28,7 @@ echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
 toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
+echo -e "\e[4;34m This Bomber Was Created By SpeedX and modified by H420 \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
 echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
 echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
@@ -58,12 +59,12 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/TheSpeedX/TBomb
-if [[ -s TBomb/TBomb.sh ]];then
-cd TBomb
+git clone https://github.com/H420Prajyot/bomber/
+if [[ -s bomber/TBomb.sh ]];then
+cd bomber
 cp -r -f * .. > temp
 cd ..
-rm -rf  TBomb >> temp
+rm -rf  Bomber >> temp
 rm update.speedx >> temp
 rm temp
 chmod +x TBomb.sh
